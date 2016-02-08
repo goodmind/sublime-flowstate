f2af3abd90c305ac2ec12f5667d369509dc70a01
goodmind <andwebar@gmail.com>
2016-02-08 13:06:42 +0500

    add log func

--- flowstate.py
+++ flowstate.py
@@ -1,13 +1,18 @@
 import sublime, sublime_plugin
 import functools
 
-import os
+def log(*args):
+    sublime.status_message(' '.join([str(arg) for arg in args]))
+    print(*args)
 
 class FlowstateCommand(sublime_plugin.EventListener):
     pending = 0
 
     def __init__(self):
-        self.s = sublime.load_settings('flowstate.sublime-settings')
+        s = sublime.load_settings('flowstate.sublime-settings')
+        self.timeout = s.get('flowstate_timeout') or 5000
+
+        log('Initialize Flowstate plugin...', s.get('flowstate_timeout'), self.timeout)
     
     def handle_timeout(self, view):
         self.pending = self.pending - 1
@@ -20,9 +25,8 @@ class FlowstateCommand(sublime_plugin.EventListener):
         self.pending = self.pending + 1
 
         # Ask for handleTimeout to be called in 1000ms
-        sublime.set_timeout(functools.partial(self.handle_timeout, view), self.s.get('flowstate_timeout'))
+        sublime.set_timeout(functools.partial(self.handle_timeout, view), self.timeout)
   
-    def on_idle(self, view):  
-        sublime.status_message("No activity in the past {:d}ms in file {:s}. Your work will be erased!".format(self.s.get('flowstate_timeout'), view.file_name()))
-
-        sublime.set_timeout(lambda: view.run_command('undo'), self.s.get('flowstate_timeout'))
+    def on_idle(self, view):
+        log("No activity in the past {:d}ms in file {:s}. Your work will be erased!".format(self.timeout, view.file_name()))
+        sublime.set_timeout(lambda: view.run_command('undo'), self.timeout)
