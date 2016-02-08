8de4f8bfa8e78a66bda8ef481d7db6c629dd1f0c
goodmind <andwebar@gmail.com>
2016-02-08 11:33:39 +0500

    implement file erase

--- flowstate.py
+++ flowstate.py
@@ -1,6 +1,8 @@
 import sublime, sublime_plugin
 import functools
 
+import os
+
 class FlowstateCommand(sublime_plugin.EventListener):
     pending = 0
 
@@ -20,5 +22,9 @@ class FlowstateCommand(sublime_plugin.EventListener):
         # Ask for handleTimeout to be called in 1000ms
         sublime.set_timeout(functools.partial(self.handle_timeout, view), self.s.get('flowstate_timeout'))
   
-    def on_idle(self, view):
-        print("No activity in the past {:d}ms in file {:s}".format(self.s.get('flowstate_timeout'), view.file_name()))
+    def on_idle(self, view):  
+        sublime.status_message("No activity in the past {:d}ms in file {:s}. Your work will be erased!".format(self.s.get('flowstate_timeout'), view.file_name()))
+
+        sublime.set_timeout(lambda: 
+            open(view.file_name(), 'w').close()
+        )
